92d31a44eed127cbc5628afa736cef74b38c0aa5
Johnny Hung <hungmasterj@gmail.com>
2022-12-26 22:35:08 -0500

    making a move

--- x/checkerscosmos/types/errors.go
+++ x/checkerscosmos/types/errors.go
@@ -11,4 +11,8 @@ var (
 	ErrInvalidBlack     = sdkerrors.Register(ModuleName, 1100, "Black address is invalid: %s")
 	ErrInvalidRed       = sdkerrors.Register(ModuleName, 1101, "Red address is invalid: %s")
 	ErrGameNotParseable = sdkerrors.Register(ModuleName, 1102, "Game cannot be parsed")
+	ErrGameNotFound = sdkerrors.Register(ModuleName, 1103, "Game by id not found")
+	ErrCreatorNotPlayer = sdkerrors.Register(ModuleName, 1104, "Message creator is not a player")
+	ErrNotPlayerTurn = sdkerrors.Register(ModuleName, 1105, "Player tried to play out of turn")
+	ErrWrongMove = sdkerrors.Register(ModuleName, 1106, "Wrong move")
 )
